af0ec89a3e61ed92f4de5de567dbeba74d37753a
Svetlana Vihrova <svihrova@inbox.ru>
2021-07-10 00:13:58 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,39 +1,85 @@
 var rotate1 = 0;
 var rotate2 = 0;
 Circle.onclick = function() {
-    var circle = document.getElementById('TropicCircle');
-    var circle2 = document.getElementById('SiderCircle');
-    var circle3 = document.getElementById('AstrolCircle');
+    var circleTrop = document.getElementById('TropicCircle');
+    var circleSider = document.getElementById('SiderCircle');
+    var circleAstr = document.getElementById('AstrolCircle');
     rotate1 = rotate1 ? 0 : 360;
     rotate2 = rotate2 ? 0 : -360;
-    circle.style.transform = "rotate(" + rotate1 + "deg)";
-    circle2.style.transform = "rotate(" + rotate2 + "deg)";
-    circle3.style.transform = "rotate(" + rotate1 + "deg)";
-    circle.style.transition = "transform 1.5s";
-    circle2.style.transition = "transform 1.5s";
-    circle3.style.transition = "transform 1.5s";
+    circleTrop.style.transform = "rotate(" + rotate1 + "deg)";
+    circleSider.style.transform = "rotate(" + rotate2 + "deg)";
+    circleAstr.style.transform = "rotate(" + rotate1 + "deg)";
+    circleTrop.style.transition = "transform 1.5s";
+    circleSider.style.transition = "transform 1.5s";
+    circleAstr.style.transition = "transform 1.5s";
     console.log("Work");
-
 };
+
+
+var date = new Date();
+date = addZero(date.getMonth()+1) + "-" + addZero(date.getDate());
+console.log(date);
+function addZero(num) {
+  if (num <= 9 ){
+  num = '0'+num;
+  }
+return num;
+}
+Date.get
+//date = "06-25";
+let zodiacsDate = [
+["Овен", '03-21', '04-20'],
+["Телец", '04-21', '05-21'],
+["Блинецы", '05-22', '06-21'],
+["Рак", '06-22', '07-22'],
+["Лев", '07-23', '08-23'],
+["Дева", '08-24', '09-23'],
+["Весы", '09-24', '10-23'],
+["Скорпион", '10-24', '11-22'],
+["Стрелец", '11-23', '12-22'],
+["Козерог", '12-23', '01-20'],
+["Водолей", '01-21', '02-19'],
+["Рыбы", '02-20', '03-20']
+];
+
+var zodiakNow;
+var current_rotation = 180;
+var alf, gradus;
+for (let i=0; i<12; i++){
+  if (date >= zodiacsDate[i][1] && date <= zodiacsDate[i][2] ) {
+    zodiakNow = zodiacsDate[i][0];
+    gradus = current_rotation - (30*i+15);
+    var radius  = 220; // радиус окружности 
+    alf = ((30*i+15) * Math.PI)/180;
+    document.querySelector("#Sun").style.transform = 'rotate(' + gradus +'deg)';
+    document.querySelector("#Sun").style.left = 1100+ radius * Math.sin(alf);
+    document.querySelector("#Sun").style.top = 360 + radius * Math.cos(alf);
+    
+    console.log(zodiacsDate[i][0]);
+    console.log("i = "+i+" gradus = " + gradus + " alf = " + alf);
+    
+  }
+}
 
 
 function animation(args, elem) { // некоторые аргументы определим на будущее
 	var $ = {
 		radius  :     220, // радиус окружности 
-		speed   :     20 // скорость/задержка ( в js это мс, например 10 мс = 100 кадров в секунду)
+		speed   :     40 // скорость/задержка ( в js это мс, например 10 мс = 100 кадров в секунду)
 	}
-  var current_rotation = 180;
-	var f = 0;
+  var rotateSun = gradus;
+	var f = alf;
 	var s = 2 * Math.PI / 180; //Вычислим угол
 	setInterval(function() { // функция движения 
 		f += s; // приращение аргумента
-    //elem.style.transform = "rotate(" + -360 + "deg)";
-    //elem.style.transition = "transform 2s";
-		  elem.style.left =  858 + $.radius * Math.sin(f)  + 'px'; // меняем координаты элемента, подобно тому как мы это делали в школе в декартовой системе координат. Правда, в данном случае используется полярная система координат, изменяя угол
-		  elem.style.top =   360 + $.radius * Math.cos(f) + 'px';
 
-      current_rotation -= 2; // rotate clockwise by 90 degrees
-      document.querySelector("#Sun").style.transform = 'rotate(' + current_rotation + 'deg)';
+		  elem.style.left =  1100 + radius * Math.sin(f) + 'px' ;//858 + $.radius * Math.sin(f)  + 'px'; // меняем координаты элемента, подобно тому как мы это делали в школе в декартовой системе координат. Правда, в данном случае используется полярная система координат, изменяя угол
+		  elem.style.top =   360 + radius * Math.cos(f) + 'px';//360 + $.radius * Math.cos(f) + 'px';
+      console.log(Math.sin(f));
+      console.log("f = "+f);
+
+      rotateSun -= 2; // rotate clockwise by 90 degrees
+      document.querySelector("#Sun").style.transform = 'rotate(' + rotateSun + 'deg)';
 	}, $.speed)
 }
 
