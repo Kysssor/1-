45bea35d7050bae8d7653ab4f616947825229374
katirenn <katirenn55001@mail.ru>
2021-07-18 17:44:52 +0300

    file structure update

--- dark-light.js
+++
@@ -1,4 +0,0 @@
-function darkLight() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
--- modules/calendarWidget.js
+++ modules/calendarWidget.js
@@ -1,25 +1,25 @@
-const dateCal = new Date();
+const DATECAL = new Date();
 
 const renderCalendar = () => {
-    dateCal.setDate(1);
+    DATECAL.setDate(1);
 
-    const monthDays = document.querySelector(".daysCal");
+    const MONTHDAYS = document.querySelector(".daysCal");
 
     //последний день текущего месяца
-    const lastDay = new Date(dateCal.getFullYear(), dateCal.getMonth() + 1, 0).getDate();
+    const LASTDAY = new Date(DATECAL.getFullYear(), DATECAL.getMonth() + 1, 0).getDate();
 
     //последний день предыдущего месяца
-    const prevLastDay = new Date(dateCal.getFullYear(), dateCal.getMonth(), 0).getDate();
+    const PREVLASTDAY = new Date(DATECAL.getFullYear(), DATECAL.getMonth(), 0).getDate();
 
     //индекс первого дня недели текущего месяца(типо чт-4,сб-6)
-    const firstDayIndex = dateCal.getDay();
+    const FIRSTDAYINDEX = DATECAL.getDay();
 
     //индекс последнего дня недели текущего месяца(типо чт-4,сб-6)
-    const lastDayIndex = new Date(dateCal.getFullYear(), dateCal.getMonth() + 1, 0).getDay();
+    const LASTDAYINDEX = new Date(DATECAL.getFullYear(), DATECAL.getMonth() + 1, 0).getDay();
 
-    const nextDays = 7 - lastDayIndex - 1;
+    const NEXTDAYS = 7 - LASTDAYINDEX - 1;
 
-    const months = [
+    const MONTHS = [
         "Январь",
         "Февраль",
         "Март",
@@ -41,37 +41,37 @@ const renderCalendar = () => {
     // } else {
     //     document.querySelector('.date h1').innerHTML = ninp;
     // }
-    document.querySelector('.date h1').innerHTML = months[dateCal.getMonth()] + " " + dateCal.getFullYear();
+    document.querySelector('.date h1').innerHTML = MONTHS[DATECAL.getMonth()] + " " + DATECAL.getFullYear();
     // document.querySelector('.date p').innerHTML = new Date().toDateString();
 
     let days = "";
     //числа предыдущего месяца
-    for (let x = firstDayIndex; x > 0; x--) {
-        days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+    for (let x = FIRSTDAYINDEX; x > 0; x--) {
+        days += `<div class="prev-date">${PREVLASTDAY - x + 1}</div>`;
     }
     //числа текущего месяца
-    for (let i = 1; i <= lastDay; i++) {
+    for (let i = 1; i <= LASTDAY; i++) {
         //определение текущего дня
-        if (i === new Date().getDate() && dateCal.getMonth() === new Date().getMonth()) {
+        if (i === new Date().getDate() && DATECAL.getMonth() === new Date().getMonth()) {
             days += `<div class="today">${i}</div>`;
         } else {
             days += `<div>${i}</div>`;
         }
     }
     //числа следующего месяца
-    for (let j = 1; j <= nextDays; j++) {
+    for (let j = 1; j <= NEXTDAYS; j++) {
         days += `<div class="next-date">${j}</div>`;
     }
-    monthDays.innerHTML = days;
+    MONTHDAYS.innerHTML = days;
 }
 //стрелка назад(предыдущий месяц)
 document.querySelector('.prev').addEventListener('click', () => {
-    dateCal.setMonth(dateCal.getMonth() - 1);
+    DATECAL.setMonth(DATECAL.getMonth() - 1);
     renderCalendar();
 })
 //стрелка вперед(следующий месяц)
 document.querySelector('.next').addEventListener('click', () => {
-    dateCal.setMonth(dateCal.getMonth() + 1);
+    DATECAL.setMonth(DATECAL.getMonth() + 1);
     renderCalendar();
 })
 renderCalendar();
