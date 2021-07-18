b2fc5a08cd18d672b90ac87568de677b5653109c
katirenn <katirenn55001@mail.ru>
2021-07-19 02:07:11 +0300

    affirmation update

--- config.rb
+++ config.rb
@@ -1,2 +1,4 @@
-css_dir =   "css"
-sass_dir =  "sass"
+css_dir = 'public'
+sass_dir = 'assets/sass'
+images_dir = 'assets/img'
+sass_options = {:cache_location => "assets/sass/.sass-cache"}
---
+++ modules/starrySky.js
@@ -0,0 +1,2 @@
+const SKY = document.querySelector(".starry-sky");
+SKY.innerHTML = `<div id='stars'></div>` + `<div id='stars2'></div>` + `<div id='stars3'></div>`;
