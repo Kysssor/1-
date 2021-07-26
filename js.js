78e1916b6084b404914bfe9f3e8686126a67f153
Svetlana Vihrova <svihrova@inbox.ru>
2021-07-26 18:39:52 +0300

    Update js.js

--- js.js
+++ js.js
@@ -17,8 +17,9 @@ Circle.onclick = function() {
 
 var date = new Date();
 date = addZero(date.getMonth()+1) + "-" + addZero(date.getDate());
-//date = "07-22";
-console.log(date);
+var year = 2021;
+//date = "07-27";
+console.log(date + " "+year);
   function addZero(num) {
     if (num <= 9 ){
     num = '0'+num;
@@ -40,6 +41,34 @@ let zodiacsDate = [
 ["Водолей", '01-21', '02-19'],
 ["Рыбы", '02-20', '03-20']
 ];
+let calendarForMoon = [
+["Овен", 0, 30],
+["Телец",31, 60],
+["Блинецы",61,90],
+["Рак",91,120],
+["Лев",121,150],
+["Дева",151,180],
+["Весы",181,210],
+["Скорпион",211,240],
+["Стрелец",241, 270],
+["Козерог",271, 300],
+["Водолей",301,330],
+["Рыбы",331,360],
+["Водолей", 328, 351],
+["Рыбы",352, 360],
+["Рыбы",0, 28],
+["Овен",29, 53],
+["Телец",54, 90],
+["Близнецы", 91,118],
+["Рак",119,138],
+["Лев",139, 174],
+["Дева",175, 218],
+["Весы",219,241],
+["Скорпион", 242, 247],
+["Змееносец", 248, 266],
+["Стрелец",267, 299],
+["Козерог", 300,327]
+];
 
 var zodiakNow, alf, gradus, alfMoon;
 var radius  = 220; // радиус окружности 
@@ -53,9 +82,6 @@ for (let i=0; i<12; i++){
     }
 }
 
-//Намётки на отыскание центра
-//const { right, left, width, height } = document.querySelector("#TropicCircle").getBoundingClientRect();
-//console.log("Размер "+ height + " "+ width + " "+ left + " " + right + " ");
 
 function moveAndRotateSun(index) {
     zodiakNow = zodiacsDate[index][0];
@@ -72,50 +98,86 @@ function moveAndRotateSun(index) {
     
     gradus = 180 - (30*index+dayInZodiak);
     alf = ((30*index+dayInZodiak) * Math.PI)/180;
+    document.getElementById("ZnakZodiak").innerHTML = "Знак зодиака: "+zodiakNow;
     moveAndRotateMoon(30*index+dayInZodiak);
     document.querySelector("#Sun").style.transform = 'rotate(' + gradus +'deg)';
     document.querySelector("#Sun").style.marginLeft = -206 + radius * Math.sin(alf);
     document.querySelector("#Sun").style.marginTop = 183 + radius * Math.cos(alf);
-    document.getElementById("ZnakZodiak").innerHTML = zodiakNow;
+    
 }
+
 var gradusMoon;
 function moveAndRotateMoon(longitudeSun) {
-  
-  var ageOfMoon = (8*11 - 14) % 30 + Number(date[0]+date[1]) + Number(date[3] + date[4]);
+  var numberMoon; //Лунное число (меняется в зависимости от года)
+  if (year > 2013 ) {
+    if (19 >= (year - 2013)){
+    numberMoon = (year - 2013);
+    } else numberMoon = (year - 2013) % 19;
+  } else  {
+    if (19 >= (2013-year)) {
+      numberMoon = 19 - (2013-year);
+    } else numberMoon = 19 - (2013-year)%19;
+  }
+  console.log(numberMoon);
+  //Вычисляем возраст луны
+  var ageOfMoon = (numberMoon*11 - 14) % 30 + Number(date[0]+date[1]) + Number(date[3] + date[4]);
   if (ageOfMoon>30) {
     ageOfMoon = ageOfMoon % 30;
   }
   var longitudeMoon = ageOfMoon * 12 + longitudeSun;
+  for (var i = 0; i<26; i++){
+    if ((longitudeMoon >= calendarForMoon[i][1] && longitudeMoon<=calendarForMoon[i][2]) || ((longitudeMoon <= calendarForMoon[i][1] && longitudeMoon>=calendarForMoon[i][2]))){
+      console.log(calendarForMoon[i] +" "+longitudeMoon);
+      if (i<12) document.getElementById("ZnakMoon").innerHTML = " Луна в знаке: " + calendarForMoon[i][0];
+      else      document.getElementById("ZnakMoon").innerHTML = document.getElementById("ZnakMoon").innerHTML + " Луна в создвездии: " + calendarForMoon[i][0];
+    }}
   gradusMoon = 180 - longitudeMoon;
   alfMoon = (longitudeMoon * Math.PI)/180;
   document.querySelector("#Moon").style.transform = 'rotate(' + gradusMoon +'deg)';
   document.querySelector("#Moon").style.marginLeft = -200 + radius * Math.sin(alfMoon);
   document.querySelector("#Moon").style.marginTop = 193 + radius * Math.cos(alfMoon);
-  console.log("Возраст "+ageOfMoon + " " + longitudeMoon + " " + longitudeSun + " " + gradusMoon);
 }
 
-function animation(args, elem) { // некоторые аргументы определим на будущее
-	var $ = {
+var flag = true;
+
+var idSetIntervalOfSun, idSetIntervalOfMoon;
+function animation() { // некоторые аргументы определим на будущее
+  if (flag){
+    flag = false;
+  var $ = {
 		speed   :     40 // скорость/задержка ( в js это мс, например 10 мс = 100 кадров в секунду)
 	}
   var rotateSun = gradus;
   var rotateMoon = gradusMoon;
 	var f = alf;
   var f2 = alfMoon;
+
 	var s = 2 * Math.PI / 180; //Вычислим угол
-	setInterval(function() { // функция движения 
+	idSetIntervalOfSun = setInterval(function() { // функция движения 
 		f += s; // приращение аргумента
-		  elem.style.marginLeft =  -206 + radius * Math.sin(f) + 'px' ;//1100 + $.radius * Math.sin(f)  + 'px'; // меняем координаты элемента, подобно тому как мы это делали в школе в декартовой системе координат. Правда, в данном случае используется полярная система координат, изменяя угол
-		  elem.style.marginTop =   183 + radius * Math.cos(f) + 'px';//360 + $.radius * Math.cos(f) + 'px';
+      document.querySelector("#Sun").style.marginLeft =  -206 + radius * Math.sin(f) + 'px' ;//1100 + $.radius * Math.sin(f)  + 'px'; // меняем координаты элемента, подобно тому как мы это делали в школе в декартовой системе координат. Правда, в данном случае используется полярная система координат, изменяя угол
+		  document.querySelector("#Sun").style.marginTop =   183 + radius * Math.cos(f) + 'px';//360 + $.radius * Math.cos(f) + 'px';
       rotateSun -= 2; // rotate clockwise by 90 degrees
       document.querySelector("#Sun").style.transform = 'rotate(' + rotateSun + 'deg)';
-	}, $.speed)
-  setInterval(function() { // функция движения 
+	}, $.speed);
+  idSetIntervalOfMoon = setInterval(function() { // функция движения 
 		f2 += s; // приращение аргумента
     document.querySelector("#Moon").style.marginLeft =  -200 + radius * Math.sin(f2) + 'px' ;//1100 + $.radius * Math.sin(f)  + 'px'; // меняем координаты элемента, подобно тому как мы это делали в школе в декартовой системе координат. Правда, в данном случае используется полярная система координат, изменяя угол
     document.querySelector("#Moon").style.marginTop =   193 + radius * Math.cos(f2) + 'px';//360 + $.radius * Math.cos(f) + 'px';
       rotateMoon -= 2; // rotate clockwise by 90 degrees
       document.querySelector("#Moon").style.transform = 'rotate(' + rotateMoon + 'deg)';
-	}, 8)
-}
+	}, 8);
+} else {
+  
+    clearInterval(idSetIntervalOfMoon);   
+    document.querySelector("#Moon").style.transform = 'rotate(' + gradusMoon +'deg)';
+    document.querySelector("#Moon").style.marginLeft = -200 + radius * Math.sin(alfMoon);
+    document.querySelector("#Moon").style.marginTop = 193 + radius * Math.cos(alfMoon);
 
+clearInterval(idSetIntervalOfSun);
+
+document.querySelector("#Sun").style.transform = 'rotate(' + gradus +'deg)';
+document.querySelector("#Sun").style.marginLeft = -206 + radius * Math.sin(alf);
+document.querySelector("#Sun").style.marginTop = 183 + radius * Math.cos(alf);
+flag = true;
+}}
